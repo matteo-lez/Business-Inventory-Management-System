6ff51db4adefbeeb9c9c1b1c004954d143f963bd
Shaelos <unconfigured@null.spigotmc.org>
2024-07-24 18:25:54 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Business_Inventory_Management_System
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
