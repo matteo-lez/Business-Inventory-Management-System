532ea17d56ff66341dd9e2a7ba98939989573c8f
Matteo <matteo.lez@gmail.com>
2024-07-28 18:15:17 +0200

    Added moving option to login window.

--- Software/View/LoginView.xaml.cs
+++ Software/View/LoginView.xaml.cs
@@ -23,5 +23,11 @@ namespace Business_Inventory_Management_System.View
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) 
+                DragMove();
+        }
     }
 }
