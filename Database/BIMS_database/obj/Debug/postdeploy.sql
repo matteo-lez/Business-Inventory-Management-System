49ccc2e873fe4790a091897532679208f59b3bc6
Matteo <matteo.lez@gmail.com>
2024-08-28 02:17:13 +0200

    Added ViewModel.

---
+++ Database/BIMS_database/obj/Debug/postdeploy.sql
@@ -0,0 +1,27 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+INSERT INTO [dbo].[User] ([Id], [Username], [Password], [Name], [LastName], [Email])
+VALUES (NEWID(), 'admin', 'admin', 'Administrator', 'BIMS', 'admin.bims@bims.com');
+
+INSERT INTO [dbo].[User] ([Id], [Username], [Password], [Name], [LastName], [Email])
+VALUES (NEWID(), 'varys', 'thespider', 'Varys', 'Unknown', 'lordvarys@got.com');
+
+INSERT INTO [dbo].[User] ([Id], [Username], [Password], [Name], [LastName], [Email])
+VALUES (NEWID(), 'baelish', 'littlefinger', 'Petyr', 'Baelish', 'lordbaelish@got.com');
+
+INSERT INTO [dbo].[User] ([Id], [Username], [Password], [Name], [LastName], [Email])
+VALUES (NEWID(), 'arya', 'nottoday', 'Arya', 'Stark', 'arya.stark@got.com');
+
+INSERT INTO [dbo].[User] ([Id], [Username], [Password], [Name], [LastName], [Email])
+VALUES (NEWID(), 'tyrion', 'theimp', 'Tyrion', 'Lannister', 'tyrion.lannister@got.com');
+GO
---
+++ Software/ViewModel/LoginViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Inventory_Management_System.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        // Fields
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+
+            set
+            {
+                _username = value;
+            }
+        }
+        public SecureString Password
+        {
+            get
+            {
+                return _password;
+            }
+
+            set
+            {
+                _password = value;
+            }
+        }
+        public string ErrorMessage
+        {
+            get
+            {
+                return _errorMessage;
+            }
+
+            set
+            {
+                _errorMessage = value;
+            }
+        }
+        public bool IsViewVisible
+        {
+            get
+            {
+                return _isViewVisible;
+            }
+
+            set
+            {
+                _isViewVisible = value;
+            }
+        }
+    }
+}
